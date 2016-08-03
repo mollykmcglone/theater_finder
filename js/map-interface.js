fdbec18ab077e2689bbd7c584b46044d7150e312
Christine Tacha and Molly McGlone <Guest@epicodus-58.local>
2016-08-03 14:46:48 -0700

    add google map to index with some styling

---
+++ js/map-interface.js
@@ -0,0 +1,10 @@
+// 
+// $(document).ready(function() {
+//   var map;
+//     function initMap() {
+//       map = new google.maps.Map(document.getElementById('map'), {
+//       center: {lat: 45.5231, lng: 122.6765},
+//       zoom: 8
+//     });
+//   }
+// });
--- js/theater.js
+++ js/theater.js
@@ -4,7 +4,7 @@ Theater = function(){
 };
 
 Theater.prototype.getTheater = function(name, displayDetails) {
-  $.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=10&type=restaurant&name=' + name + '&key=' + apiKey).then(function(response) {
+  $.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=45.5231,122.6765&radius=10&type=movie_theater&name=' + name + '&key=' + apiKey).then(function(response) {
     console.log(response);
     // displayDetails(city, response.main.name);
     // displayDetails(city, response.main.address);
